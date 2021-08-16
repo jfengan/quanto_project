3927d3abd08589c84ba4e1a33e530ae36d90a8c9
jiahaofeng <jfengan@connet.ust.hk>
2021-08-16 09:05:19 +0800

    analytical pricing

--- digital_quanto.py
+++ digital_quanto.py
@@ -1,53 +1,72 @@
-import math
 from abc import ABC, abstractmethod
 from scipy.stats import norm
+import math
+
 
 
 class DigitalQuanto(ABC):
-    def __init__(self, spot, strike, t, rho, sigma_spot, sigma_fx, r_quote, r_base, r_dom):
+    def __init__(self, rs, q, rf, sigma_s, sigma_f, rho, t, spot, strike, quanto_factor):
+        self.rs = rs
+        self.rf = rf
+        self.q = q
+        self.sigma_s = sigma_s
+        self.sigma_f = sigma_f
+        self.rho = rho
+        self.t = t
         self.spot = spot
         self.strike = strike
-        self.t = t
-        self.rho = rho
-        self.sigma_spot = sigma_spot
-        self.sigma_fx = sigma_fx
-        self.r_quote = r_quote
-        self.r_base = r_base
-        self.r_dom = r_dom
+        self.quanto_factor = quanto_factor
 
     @abstractmethod
     def price(self):
         raise NotImplemented
 
 
-class DigitalQuantoCall(DigitalQuanto):
+class FloatingFXDigitalQuantoCall(DigitalQuanto):
 
-    def sigma_spot_t(self):
-        return self.r_base - self.r_quote - self.rho * self.sigma_spot * self.sigma_fx
+    def dom_drift(self):
+        # Because we let the exchange rate be floating, so there's no risk-adjustment for the drift
+        # since under fx measure, the payoff is fixed -- 1 fx currency
+        return self.rs - self.q - 0.5 * self.sigma_s ** 2
 
-    def d(self):
-        return (math.log(self.spot / self.strike) + (self.sigma_spot_t() - self.sigma_spot ** 2 / 2) * self.t) \
-               / (self.sigma_spot * math.sqrt(self.t))
-
-    def price(self):
-        return math.exp(-self.r_dom * self.t) * norm.cdf(self.d())
-
-
-class DigitalQuantoPut(DigitalQuantoCall):
+    def d1(self):
+        return (math.log(self.spot / self.strike) + self.dom_drift() * self.t) / (self.sigma_s * math.sqrt(self.t))
 
     def price(self):
-        return 1 - super().price()
+        return math.exp(-self.rf * self.t) * norm.cdf(self.d1())
+
+
+class FloatingFXDigitalQuantoPut(FloatingFXDigitalQuantoCall):
+
+    def price(self):
+        return 1 - super(FloatingFXDigitalQuantoPut, self).price()
+
+
+class FixedFXDigitalQuantoCall(DigitalQuanto):
+
+    def dom_drift(self):
+        return self.rs - self.q - 0.5 * self.sigma_s ** 2
+
+    def price(self):
+        return
+
+    
+class FixedFXDigitalQuantoPut(DigitalQuanto):
+
+    def price(self):
+        return 1 - super(FixedFXDigitalQuantoPut, self).price()
 
 
 if __name__ == "__main__":
-    spot = 106.6
+    spot = 106
     strike = 110
-    t = 1
-    rho = -0.2789
-    sigma_spot = 0.0855
-    sigma_fx = 0.1099
-    r_base = 0.025
-    r_quote = 0.001
-    r_dom = 0.04
-    call = DigitalQuantoCall(spot, strike, t, rho, sigma_spot, sigma_fx, r_quote, r_base, r_dom)
-    print(1 - call.price())
+    tau = 0.5
+    q = 0.
+    rs = 0.04
+    rf = 0.05
+    sigma_s = 0.4
+    sigma_f = 0.08
+    rho = -0.1
+
+    ffdqcall = FloatingFXDigitalQuantoCall(rs, q, rf, sigma_s, sigma_f, rho, tau, spot, strike, "")
+    print(ffdqcall.price())
